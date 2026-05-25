07c5a96e2e18b451f47b2c2c14d0d6f0b102950d
Jose Macciotti <jmacciottineto@gmail.com>
2026-05-25 20:44:42 -0300

    FEAT: Add basic profile and home pages

---
+++ app/(private)/profile.tsx
@@ -0,0 +1,10 @@
+import { ScreenWrapper } from "@/components/ScreenWraper";
+import { Text } from "react-native";
+
+export default function Profile() {
+  return (
+    <ScreenWrapper>
+      <Text className="text-white text-xl">Home Page do Kombinado</Text>
+    </ScreenWrapper>
+  );
+}
