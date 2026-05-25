7f0b1afd266872734539d2f898e65843458e5e3d
Jose Macciotti <jmacciottineto@gmail.com>
2026-05-25 20:47:52 -0300

    FEAT: Add fake router to App layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,13 @@
 import { Stack } from "expo-router";
-import { AuthProvider, useAuth } from "../context/auth/AuthProvider";
+import { AuthProvider } from "../context/auth/AuthProvider";
 import "../global.css";
-
-// Criamos um componente interno para poder usar o hook 'useAuth' com segurança
-function RootNavigation() {
-  const { userToken } = useAuth();
-
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      {/* Se NÃO está logado, a única rota existente na árvore é o grupo (auth) */}
-      {!userToken ? (
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      ) : (
-        // Se está logado, o app esquece o login e monta apenas o grupo privado (app)
-        <Stack.Screen name="(app)" options={{ headerShown: false }} />
-      )}
-    </Stack>
-  );
-}
 
 export default function RootLayout() {
   return (
     <AuthProvider>
-      <RootNavigation />
+      <Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
     </AuthProvider>
   );
 }
