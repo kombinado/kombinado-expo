a4cbf8fa4edc9180424274c7699a300bdb690471
Jose Macciotti <jmacciottineto@gmail.com>
2026-05-27 17:25:42 -0300

    FEAT: Add ToggleButton component

--- src/app/(private)/home.tsx
+++ src/app/(private)/home.tsx
@@ -1,10 +1,28 @@
+import { SearchBar } from "@/src/components/feature/SearchBar";
 import { ScreenWrapper } from "@/src/components/layout/ScreenWraper";
-import { Text } from "react-native";
+import { ToggleButton } from "@/src/components/ui/ToggleButton";
+import { useState } from "react";
+import { Text, View } from "react-native";
 
 export default function Home() {
+  const [isToRequest, setisToRequest] = useState(false);
+  const [destiny, setDestiny] = useState("");
+
   return (
     <ScreenWrapper>
-      <Text>/</Text>
+      <View className="mb-10">
+        <Text className="text-2xl font-bold">Caronas Disponíveis</Text>
+      </View>
+
+      <View className="mb-10">
+        <ToggleButton value={isToRequest} onToggle={setisToRequest} />
+      </View>
+
+      <SearchBar
+        value={destiny}
+        placeholder="Pra onde vamos?"
+        onChangeText={setDestiny}
+      />
     </ScreenWrapper>
   );
 }
