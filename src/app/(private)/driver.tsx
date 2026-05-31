62a843057296bbe6de1b954cf32044b585b0be8b
Jose Macciotti <jmacciottineto@gmail.com>
2026-05-31 09:37:49 -0300

    FEAT: Add Driver Ride Card component

--- src/app/(private)/driver.tsx
+++ src/app/(private)/driver.tsx
@@ -1,10 +1,24 @@
+import { DriverRideCard } from "@/src/components/feature/DriverRideCard";
 import { ScreenWrapper } from "@/src/components/layout/ScreenWraper";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Driver() {
   return (
     <ScreenWrapper>
-      <Text className="text-white text-xl">Home Page do Kombinado</Text>
+      <View className="mb-10">
+        <Text className="text-2xl font-bold">Gerenciar Caronas</Text>
+      </View>
+
+      <DriverRideCard
+        date="01/06"
+        time="21h30"
+        origin="Campus IFTM"
+        destination="Terminal Oeste"
+        occupiedSpots={2}
+        totalSpots={4}
+        onCancelRide={() => console.log("Cancelando Carona")}
+        onViewRequests={() => console.log("Analisando solicitações")}
+      />
     </ScreenWrapper>
   );
 }
